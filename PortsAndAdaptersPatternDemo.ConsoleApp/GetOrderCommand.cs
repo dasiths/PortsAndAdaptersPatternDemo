abbcffc950cf0762acff4b9293c44828839ce6cf
Dasith Wijes <dasiths@hotmail.com>
2023-05-12 03:06:40 +1000

    console command using mediatr

--- PortsAndAdaptersPatternDemo.ConsoleApp/GetOrderCommand.cs
+++ PortsAndAdaptersPatternDemo.ConsoleApp/GetOrderCommand.cs
@@ -1,16 +1,35 @@
-﻿using Spectre.Console.Cli;
+﻿using System.Text.Json;
+using MediatR;
+using PortsAndAdaptersPatternDemo.RequestProcessing.Features.GetOrder;
+using Spectre.Console.Cli;
 
 internal class GetOrderCommand : Command<GetOrderCommand.Settings>
 {
+    private readonly IMediator _mediator;
+
     public class Settings : CommandSettings
     {
         [CommandArgument(0, "[OrderId]")]
-        public string OrderId { get; set; }
+        public int OrderId { get; set; }
     }
-
+    
+    public GetOrderCommand(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
 
     public override int Execute(CommandContext context, Settings settings)
     {
+        var result = _mediator.Send(new GetOrderRequest()
+        {
+            OrderId = settings.OrderId
+        }).ConfigureAwait(false).GetAwaiter().GetResult();
+
+        Console.WriteLine(JsonSerializer.Serialize(result, new JsonSerializerOptions()
+        {
+            WriteIndented = true
+        }));
+
         return 0;
     }
 }
--- PortsAndAdaptersPatternDemo.ConsoleApp/GetProductCommand.cs
+++ PortsAndAdaptersPatternDemo.ConsoleApp/GetProductCommand.cs
@@ -1,4 +1,7 @@
 ﻿using System.Diagnostics.CodeAnalysis;
+using System.Text.Json;
+using MediatR;
+using PortsAndAdaptersPatternDemo.RequestProcessing.Features.GetProduct;
 using Spectre.Console.Cli;
 
 internal sealed class GetProductCommand : Command<GetProductCommand.Settings>
@@ -6,11 +9,28 @@ internal sealed class GetProductCommand : Command<GetProductCommand.Settings>
     public sealed class Settings : CommandSettings
     {
         [CommandArgument(0, "[ProductId]")]
-        public string ProductId { get; set; }
+        public int ProductId { get; set; }
+    }
+
+    private readonly IMediator _mediator;
+
+    public GetProductCommand(IMediator mediator)
+    {
+        _mediator = mediator;
     }
 
     public override int Execute([NotNull] CommandContext context, [NotNull] Settings settings)
     {
+        var result = _mediator.Send(new GetProductRequest()
+        {
+            ProductId = settings.ProductId
+        }).ConfigureAwait(false).GetAwaiter().GetResult();
+
+        Console.WriteLine(JsonSerializer.Serialize(result, new JsonSerializerOptions()
+        {
+            WriteIndented = true
+        }));
+
         return 0;
     }
 }
